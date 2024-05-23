6aa04c0d3316d38e0413965643eb65928b01c335
Ryan Tan <ryantan.mail@gmail.com>
2024-05-23 12:28:01 -0700

    lol css is another beast of itself to do, made the background the color I wanted

--- birdiepicks/src/app/page.tsx
+++ birdiepicks/src/app/page.tsx
@@ -1,8 +1,8 @@
-
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-		<h1> hello birdiepicks </h1>
-    </main>
+	<main className="flex min-h-screen items-center justify-center">
+		<h1> Birdie Picks </h1>
+		<h1> Textbox placeholder </h1>
+	</main>
   );
 }
