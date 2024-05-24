a7ff64ad779d456215b1f515d8f5608e27eb2537
Ryan Tan <ryantan.mail@gmail.com>
2024-05-23 22:20:48 -0700

    background color added, more css, using a combo box next for search

--- birdiepicks/src/app/layout.tsx
+++ birdiepicks/src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const robotoMono = Roboto_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Birdiepicks",
@@ -12,7 +12,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode;}>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={robotoMono.className}>{children}</body>
     </html>
   );
 }
