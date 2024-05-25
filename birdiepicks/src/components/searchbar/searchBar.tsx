5101fc9197337b4fa6eabba73826f7d3e3a27375
Ryan Tan <ryantan.mail@gmail.com>
2024-05-24 23:53:37 -0700

    added a combobox, update some readme

--- birdiepicks/src/components/searchbar/searchBar.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-import { useState } from "react"
-
-export default function SearchBar() {
-	
-	return(
-		<input type="text" className="min-w-half w-5/12 h-vh bg-search-grey rounded border-2 border-black"></input>
-	)
-}
