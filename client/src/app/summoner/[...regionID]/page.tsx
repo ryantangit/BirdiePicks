1b49290a8db15a0b7d645a53450829a5647c01a1
Ryan Tan <ryantan.mail@gmail.com>
2024-05-26 15:18:00 -0700

    decided to roll with a go backend in the end after back and forth

---
+++ client/src/app/summoner/[...regionID]/page.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Page({params}: {params: {regionID: string[]}}) {
+	return (
+	<div>
+		<h1> Summoner Name: {params.regionID[1]}</h1>
+		<h1> Region: {params.regionID[0]}</h1>
+	</div>
+	)
+
+} 
