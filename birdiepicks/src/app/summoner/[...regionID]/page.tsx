4473a14805e327a0b57b62f2336c2579a2b572b5
Ryan Tan <ryantan.mail@gmail.com>
2024-05-30 00:39:38 -0700

    added Todos

--- birdiepicks/src/app/summoner/[...regionID]/page.tsx
+++ birdiepicks/src/app/summoner/[...regionID]/page.tsx
@@ -1,11 +1,10 @@
 
-
-export default function Page({params}: {params: {regionID: string[]}}) {
-	return (
-	<div>
-		<h1> Summoner Name: {params.regionID[1]}</h1>
-		<h1> Region: {params.regionID[0]}</h1>
-	</div>
-	)
+export default function Page({ params }: { params: { regionID: string[] } }) {
+  return (
+    <div>
+      <h1> Summoner Name: {params.regionID[1]}</h1>
+      <h1> Region: {params.regionID[0]}</h1>
+    </div>
+  )
 
 } 
