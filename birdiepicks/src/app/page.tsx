f4401f1beb3b4cdea5390dfa67cf954bf113ec67
Ryan Tan <ryantan.mail@gmail.com>
2024-05-31 21:08:56 -0700

    new css utility, change in wording

--- birdiepicks/src/app/page.tsx
+++ birdiepicks/src/app/page.tsx
@@ -4,9 +4,10 @@ import { SearchBar } from "@/components";
 export default function Home() {
   return (
     <main>
-      <div className="grid grid-cols-1 min-h-screen justify-items-center content-center">
-        <h1 className="text-7xl font-main-grey p-10"> Birdie Picks </h1>
-        <div className="grid grid-cols-2">
+      <div className="grid grid-cols-1 grid-rows-5 justify-items-center content-center">
+        <h1 className="row-start-2 text-7xl standard-border font-main-grey p-10 m-2"> Birdie Picks </h1>
+        <p className="row-start-3"> A League of Legends Statistic Tracker</p>
+        <div className="row-start-4">
           <SearchBar />
         </div>
       </div>
