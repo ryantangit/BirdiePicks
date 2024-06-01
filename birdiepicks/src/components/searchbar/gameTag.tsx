cd9e27ef5deef8aa8f75aa3d47ca489f0cd40602
Ryan Tan <ryantan.mail@gmail.com>
2024-05-31 17:11:33 -0700

    continue with searchButton development, tailwind css configs

--- birdiepicks/src/components/searchbar/gameTag.tsx
+++ birdiepicks/src/components/searchbar/gameTag.tsx
@@ -1,26 +1,35 @@
 "use client"
-import { useState, useEffect } from "react"
+import { Dispatch, SetStateAction, useEffect, useId } from "react"
 import { RegionType, RegionDataManagement } from "@/utils/RegionDataManagement"
 
+
 interface GameTagProps {
-  selectedRegion: RegionType
+  gametag: string;
+  selectedRegion: RegionType;
+  setGametag: Dispatch<SetStateAction<string>>
 }
 
 const regionDataManagement = new RegionDataManagement();
 
 export default function GameTag(props: GameTagProps) {
-  const [gametag, setGametag] = useState(regionDataManagement.defaultGametag(props.selectedRegion));
   const handleInputGametag = (event: React.ChangeEvent<HTMLInputElement>) => {
     const inputElement = event.target as HTMLInputElement;
-    setGametag(inputElement.value);
+    props.setGametag(inputElement.value);
   }
+  const gameTagInputHTMLId = useId();
 
   useEffect(() => {
-    setGametag(regionDataManagement.defaultGametag(props.selectedRegion));
+    props.setGametag(regionDataManagement.defaultGametag(props.selectedRegion));
   }, [props.selectedRegion])
 
   return (
-    <input value={gametag} onChange={handleInputGametag} />
+    <div>
+      <label htmlFor={gameTagInputHTMLId}># </label>
+      <input id={gameTagInputHTMLId}
+        value={props.gametag}
+        onChange={handleInputGametag}
+        className="w-5/12 h-8 p-1 bg-search-grey standard-border" />
+    </div>
   )
 }
 
