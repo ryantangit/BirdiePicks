85f59ba2bf48bd99ae3a4ff4ef6faa8105abee75
Ryan Tan <ryantan.mail@gmail.com>
2024-06-25 18:19:54 -0700

    querying each match correctly

--- birdiepicks/src/components/MatchHistory/Match/MatchFrame.tsx
+++ birdiepicks/src/components/MatchHistory/Match/MatchFrame.tsx
@@ -1,13 +1,20 @@
+import { RiotQuery } from "@/utils/RiotQuery";
 
 
 interface MatchFrameProps {
   matchId: string;
+  regionRoute: string;
 }
 
-export default function MatchFrame(props: MatchFrameProps) {
+export default async function MatchFrame(props: MatchFrameProps) {
+  const riotQuery = new RiotQuery();
+  const matchInfo = await riotQuery.matchQuery.getMatchInfo(props.matchId, props.regionRoute);
+  console.log(matchInfo);
   return (
     <>
       {props.matchId}
     </>
   )
 }
+
+
--- birdiepicks/src/components/MatchHistory/MatchHistory.tsx
+++ birdiepicks/src/components/MatchHistory/MatchHistory.tsx
@@ -9,7 +9,7 @@ interface MatchHistoryProps {
 
 export default async function MatchHistory(props: MatchHistoryProps) {
   const riotQuery = new RiotQuery();
-  const matchQuery = await riotQuery.matchQuery.getLast20Matches(props.puuid, props.routeRegion);
+  const matchQuery = await riotQuery.matchQuery.getLast10Matches(props.puuid, props.routeRegion);
   if (!matchQuery || matchQuery.length === 0) {
     return <p> No match found </p>
   }
@@ -17,7 +17,7 @@ export default async function MatchHistory(props: MatchHistoryProps) {
     <ul>
       {matchQuery.map((matchId: string) => (
         <li key={matchId}>
-          <MatchFrame matchId={matchId} />
+          <MatchFrame matchId={matchId} regionRoute={props.routeRegion} />
         </li>
       ))}
     </ul>
