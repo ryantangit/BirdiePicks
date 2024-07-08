0d3acea4705fec486cf6b131215d1e9db7d03725
Ryan Tan <ryantan.mail@gmail.com>
2024-07-07 22:48:45 -0700

    refactored comdragon fetch, new assets endpoints

--- birdiepicks/src/components/MatchHistory/Match/MatchFrame.tsx
+++ birdiepicks/src/components/MatchHistory/Match/MatchFrame.tsx
@@ -1,5 +1,5 @@
 import { RiotQuery } from "@/utils/RiotQuery";
-import Image from "next/image";
+import MatchPersonal from "./MatchPersonal";
 
 interface MatchFrameProps {
   matchId: string;
@@ -17,23 +17,13 @@ export default async function MatchFrame(props: MatchFrameProps) {
       </>
     )
   }
-  const championIconURL = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons/";
   return (
     <>
       <p> {props.matchId} </p>
       <p> {matchInfo.queueType} </p>
       <p> {matchInfo.timeEnded} </p>
       <p> {matchInfo.gameDuration} </p>
-      <p> {matchInfo.kills}/{matchInfo.deaths}/{matchInfo.assists} </p>
-      <Image
-        src={`${championIconURL}/${matchInfo.championId}.png`}
-        alt="championIcon image"
-        height={50}
-        width={50}>
-      </Image >
-      <Image
-      >
-      </Image>
+      <MatchPersonal individual={matchInfo.individual} />
     </>
   )
 }
--- birdiepicks/src/utils/CommunityDragonQuery/CommunityDragonQuery.ts
+++ birdiepicks/src/utils/CommunityDragonQuery/CommunityDragonQuery.ts
@@ -1,2 +1,39 @@
+import itemJson from "@/CDragonJson/items.json";
+import sumSpellJson from "@/CDragonJson/summoner-spells.json"
 
-class CommDragonQuery
+interface ImageData {
+  imageHeight: number;
+  imageWidth: number;
+  imageAlt: string;
+  imageSrc: string;
+}
+
+export class CommDragonQuery {
+
+  static summonerIconImage(iconId: number) {
+    const ICON_API_PATH = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/profile-icons";
+    const imageData: ImageData = {
+      imageHeight: 150,
+      imageWidth: 150,
+      imageAlt: `Summoner Icon ${iconId}`,
+      imageSrc: `${ICON_API_PATH}/${iconId}.jpg`
+    }
+    return imageData;
+  }
+
+  static championIconImage(championId: number) {
+    const ICON_PATH = "https://raw.communitydragon.org/latest/plugins/rcp-be-lol-game-data/global/default/v1/champion-icons";
+    const imageData: ImageData = {
+      imageHeight: 60,
+      imageWidth: 60,
+      imageAlt: `Champion Icon ${championId}`,
+      imageSrc: `${ICON_PATH}/${championId}.png`
+    }
+    return imageData;
+  }
+
+  static sumSpellIconImage(spellId: number) {
+    const summonerSpellInfo = sumSpellJson.find((summoner) => summoner.id === spellId);
+    return summonerSpellInfo?.name
+  }
+}
