90534ad0cc1c323738f13ec3c995eecaa5e49b5d
Ryan Tan <ryantan.mail@gmail.com>
2024-09-12 14:23:35 -0700

    update readme

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
     To use the repo, clone and create a .env inside /birdiepicks and populate with RIOT_API
 
     Then either use docker compose up for fast build of production server or npm run dev for development
+
+    ![Front Page](./Page0.png)
+    ![Display Page](./Page1.png)
--- birdiepicks/src/app/page.tsx
+++ birdiepicks/src/app/page.tsx
@@ -1,11 +1,13 @@
-
 import { SearchBar } from "@/components";
 
 export default function Home() {
   return (
     <main>
       <div className="grid grid-cols-1 grid-rows-5 justify-items-center content-center">
-        <h1 className="row-start-2 text-7xl standard-border font-main-grey p-10 m-2"> Birdie Picks </h1>
+        <h1 className="row-start-2 text-7xl standard-border font-main-grey p-10 m-2">
+          {" "}
+          Birdie Picks{" "}
+        </h1>
         <p className="row-start-3"> A League of Legends Statistic Tracker</p>
         <div className="row-start-4">
           <SearchBar />
